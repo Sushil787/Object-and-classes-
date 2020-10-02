57d5774fa7c36a502c9040728b2e1d4f31455a38
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2020-10-02 21:51:46 +0545

    Create objectPointer.cpp

---
+++ objectPointer.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class Demo{
+private:
+int a;
+public:
+void set(int x){
+ a = x;
+}
+void sum(Demo* o1, Demo* o2){
+a = o1->a - 1;
+
+}
+
+void print(){
+cout<<"A is :"<<a<<endl;
+}
+};
+
+int main(){
+Demo o1,o2,o3;
+o1.set(10);
+o2.set(30);
+o3.sum(&o1,&o2);
+o1.print();
+o2.print();
+o3.print();
+return 0;
+}

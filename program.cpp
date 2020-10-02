03d8e1abbc6a207a57c3f55c2eec9c14acecb29f
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2020-10-02 16:45:59 +0545

    Add files via upload

--- program.cpp
+++ program.cpp
@@ -1,31 +1,28 @@
-using namespace std;
 #include<iostream>
-#include<cstring>
- class Book{
-     public:
-     int book_id ;
-     string book_name;
-
-void input_book_data(){
-    cout<<"input book ID: "<<endl;
-    cin>>book_id;
-    cout<<"input book name: "<<endl;
-    cin>>book_name;
-
-
+using namespace std;
+class Demo{
+private:
+int a;
+public:
+void set(int x){
+ a = x;
 }
-void get_data(){
-     cout<<"the book ID is : "<<book_id<<endl;
-     cout<<"the book name is : "<<book_name<<endl<<endl;
-     
+void sum(Demo o1, Demo o2){
+a = o1.a + o2.a;
 }
 
+void print(){
+cout<<"A is :"<<a<<endl;
+}
+};
 
- };
-
- int main(){
-     Book b1;
-     b1.input_book_data();
-     b1.get_data();
-     return 0;
- }
+int main(){
+Demo o1,o2,o3;
+o1.set(10);
+o2.set(30);
+o3.sum(o1,o2);
+o1.print();
+o2.print();
+o3.print();
+return 0;
+}

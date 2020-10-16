b315341b1f1cd9783b2217699f7f2b5425b5ca6a
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2020-10-16 15:31:50 +0545

    Create Making_Own_Celi_floor_function.cpp

---
+++ Making_Own_Celi_floor_function.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+//#include<sushilgyawali.h>
+int celi(float x);
+int floor(float x);
+using namespace std;
+int main(){
+    int x;
+    cout<<"input the no you like: "<<endl;
+    cin>>x;
+    cout<<"celi is: "<<celi(x)<<endl;
+    cout<<"floor is : "<<floor(x)<<endl;
+}
+
+int celi(float x){
+    return int(x);
+}
+int floor(float x){
+    return int(x)+1;
+}

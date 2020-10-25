18b7db2746588ac361f9c7217d8b51f3bf00a32d
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2020-10-25 16:31:58 +0545

    Update program.cpp

--- Array and  Vector/program.cpp
+++ Array and  Vector/program.cpp
@@ -1,1 +1,9 @@
-
+#include<iostream>
+#include<vector>
+int main(){
+    std::vector<int> data = {1,2,3,4};
+    data.push_back(12);
+    std::cout<<data[data.size()-1] <<std::endl;
+    return 0;
+    
+}
